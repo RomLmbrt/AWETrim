162c4382a8273793584f7c08c5ff2bba10393630
Theophile Dubois <t.dubois@kitepower.nl>
2025-09-25 16:23:45 +0200

    Least Square Fitting of Lisajous is done

--- src/picawe/kinematics/Lisajous_data_processing.py
+++ src/picawe/kinematics/Lisajous_data_processing.py
@@ -1,10 +1,8 @@
-from picawe.kinematics.parametrized_patterns import CST_Lissajous
 from picawe.kinematics.ReelInBspline_data_processing import ReelInBspline_data_processing as ribdata
 import numpy as np
-import scipy as sp
 import matplotlib.pyplot as plt
 
-class ReelOutLoopDefinition(ribdata):
+class Lisajous_data_processing(ribdata):
     def __init__(self, file_path_cycle=None, file_path_full=None, cyc_idx=0):
         super().__init__(file_path_cycle=file_path_cycle, file_path_full=file_path_full, cyc_idx=cyc_idx)
 
@@ -73,28 +71,10 @@ class ReelOutLoopDefinition(ribdata):
         if start_point_found == False:
             raise ValueError("No valid start point found for the CST Lissajous pattern in the reel-out data.")
 
-# if __name__ == "__main__":
-#     full_path = "/home/theophile/src/Simulation_Results/trial_Uri_valid_2/ProtoLogger_csv/2025-09-25_11-48-58_ProtoLogger.csv"
-#     cycle_path = "/home/theophile/src/Simulation_Results/trial_Uri_valid_2/cycles/cycle_data_sheet_lines.csv"
+if __name__ == "__main__":
+    full_path = "/home/theophile/src/Simulation_Results/trial_Uri_valid_2/ProtoLogger_csv/2025-09-25_11-48-58_ProtoLogger.csv"
+    cycle_path = "/home/theophile/src/Simulation_Results/trial_Uri_valid_2/cycles/cycle_data_sheet_lines.csv"
 
-#     obj = ReelOutLoopDefinition(file_path_cycle=cycle_path, file_path_full=full_path, cyc_idx=0)
-#     obj.plot_reel_out_path2D()
-#     obj.plot_reel_out_path3D()
-
-class LSQ_reel_out(ReelOutLoopDefinition):
-    def __init__(self, file_path_cycle=None, file_path_full=None, cyc_idx=0):
-        super().__init__(file_path_cycle=file_path_cycle, file_path_full=file_path_full, cyc_idx=cyc_idx)
-
-        self.az_data = self.az_Lisajous
-        self.el_data = self.el_Lisajous
-
-        # initial guess
-
-        # lower and upper bounds
-
-        # residual
-
-        # scipy least squares
-
-        # results
-
+    obj = Lisajous_data_processing(file_path_cycle=cycle_path, file_path_full=full_path, cyc_idx=0)
+    obj.plot_reel_out_path2D()
+    obj.plot_reel_out_path3D()
